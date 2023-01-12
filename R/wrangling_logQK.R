839b71c69a5ff34b0c2b64c217998ed6c82015ff
Anıl Axel Tellbüscher <anil@tellbuescher.online>
2023-01-12 12:52:15 +0100

    Using here(); removed filter for logQK values

--- R/wrangling_logQK.R
+++ R/wrangling_logQK.R
@@ -3,9 +3,6 @@
   rename(
     "logQK" = Sat..index
   ) %>% 
-   
-   filter(logQK <= 0 & logQK > -0.1) %>% 
-
   
   mutate(
     cation = if_else(Col5 == "H+1", if_else(Col8 > Col6 & Col9 != "H2O", Col9, Col7), Col5),
@@ -14,4 +11,4 @@
     anion = if_else(anion == "H2O", if_else(Col9 %in% c("H+1", "Mg+2", "Ca+2", "Fe+3"), anion, Col9), anion)
   ) %>% 
    
-   write_csv("results/logQK.csv")
+   write_csv(here::here("results", "logQK.csv"))
